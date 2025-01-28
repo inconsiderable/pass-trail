inconsiderable/pass-thread

generalise cruzbit for abstract consideration

checkpoints.go
@@ -1,0 +1,33 @@
+package passthread
+
+import (
+	"fmt"
+)
+
+// CheckpointsEnabled can be disabled for testing.
+const CheckpointsEnabled = false
+
+// LatestCheckpointHeight is used to determine if the client is synced.
+const LatestCheckpointHeight = 0
+
+// Checkpoints are known height and pass ID pairs on the main thread.
+var Checkpoints map[int64]string = map[int64]string{
+	
+}
+
+// CheckpointCheck returns an error if the passed height is a checkpoint and the
+// passed pass ID does not match the given checkpoint pass ID.
+func CheckpointCheck(id PassID, height int64) error {
+	if !CheckpointsEnabled {
+		return nil
+	}
+	checkpointID, ok := Checkpoints[height]
+	if !ok {
+		return nil
+	}
+	if id.String() != checkpointID {
+		return fmt.Errorf("Pass %s at height %d does not match checkpoint ID %s",
+			id, height, checkpointID)
+	}
+	return nil
+}

consideration_test.go
@@ -1,0 +1,145 @@
+package passthread
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"testing"
+
+	"golang.org/x/crypto/ed25519"
+)
+
+func TestConsideration(t *testing.T) {
+	// create a sender
+	pubKey, privKey, err := ed25519.GenerateKey(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// create a recipient
+	pubKey2, privKey2, err := ed25519.GenerateKey(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// create the unsigned consideration
+	tx := NewConsideration(pubKey, pubKey2, 0, 0, 0, "for lunch")
+
+	// sign the consideration
+	if err := tx.Sign(privKey); err != nil {
+		t.Fatal(err)
+	}
+
+	// verify the consideration
+	ok, err := tx.Verify()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !ok {
+		t.Errorf("Verification failed")
+	}
+
+	// re-sign the consideration with the wrong private key
+	if err := tx.Sign(privKey2); err != nil {
+		t.Fatal(err)
+	}
+
+	// verify the consideration (should fail)
+	ok, err = tx.Verify()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if ok {
+		t.Errorf("Expected verification failure")
+	}
+}
+
+func TestConsiderationTestVector1(t *testing.T) {
+	// create consideration for Test Vector 1
+	pubKeyBytes, err := base64.StdEncoding.DecodeString("80tvqyCax0UdXB+TPvAQwre7NxUHhISm/bsEOtbF+yI=")
+	if err != nil {
+		t.Fatal(err)
+	}
+	pubKey := ed25519.PublicKey(pubKeyBytes)
+
+	pubKeyBytes2, err := base64.StdEncoding.DecodeString("YkJHRtoQDa1TIKhN7gKCx54bavXouJy4orHwcRntcZY=")
+	if err != nil {
+		t.Fatal(err)
+	}
+	pubKey2 := ed25519.PublicKey(pubKeyBytes2)
+
+	tx := NewConsideration(pubKey, pubKey2, 0, 0, 0, "for lunch")
+	tx.Time = 1558565474
+	tx.Nonce = 2019727887
+
+	// check JSON matches test vector
+	txJson, err := json.Marshal(tx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(txJson) != `{"time":1558565474,"nonce":2019727887,"from":"80tvqyCax0UdXB+TPvAQwre7NxUHhISm/bsEOtbF+yI=",`+
+		`"to":"YkJHRtoQDa1TIKhN7gKCx54bavXouJy4orHwcRntcZY=","memo":"for lunch","series":1}` {
+		t.Fatal("JSON differs from test vector: " + string(txJson))
+	}
+
+	// check ID matches test vector
+	id, err := tx.ID()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if id.String() != "04c5193340be556888ef4e1c2bdad865b83b01aa637381a382afbdf1abaedb5f" {
+		t.Fatalf("ID %s differs from test vector", id)
+	}
+
+	// add signature from test vector
+	sigBytes, err := base64.StdEncoding.DecodeString("i3XHtB9CrWFB/B3UBNBFQRZD236NNZjvIBfvFPlKyFccW4BLwZ/xBZyxAzrRfY7TwbzsuMKxh5+oGgxx9FTzDw==")
+	if err != nil {
+		t.Fatal(err)
+	}
+	tx.Signature = Signature(sigBytes)
+
+	// verify the consideration
+	ok, err := tx.Verify()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !ok {
+		t.Errorf("Verification failed")
+	}
+
+	// re-sign the consideration with private key from test vector
+	privKeyBytes, err := base64.StdEncoding.DecodeString("EBQtXb3/Ht6KFh8/+Lxk9aDv2Zrag5G8r+dhElbCe07zS2+rIJrHRR1cH5M+8BDCt7s3FQeEhKb9uwQ61sX7Ig==")
+	if err != nil {
+		t.Fatal(err)
+	}
+	privKey := ed25519.PrivateKey(privKeyBytes)
+	if err := tx.Sign(privKey); err != nil {
+		t.Fatal(err)
+	}
+
+	// verify the consideration
+	ok, err = tx.Verify()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !ok {
+		t.Errorf("Verification failed")
+	}
+
+	// re-sign the consideration with the wrong private key
+	_, privKey2, err := ed25519.GenerateKey(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := tx.Sign(privKey2); err != nil {
+		t.Fatal(err)
+	}
+
+	// verify the consideration (should fail)
+	ok, err = tx.Verify()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if ok {
+		t.Errorf("Expected verification failure")
+	}
+}

genesis-pass/main.go
@@ -1,0 +1,81 @@
+package main
+
+import (
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"math/rand"
+	"time"
+
+	. "github.com/inconsiderable/pass-thread"
+	"golang.org/x/crypto/ed25519"
+)
+
+// Weave a genesis pass
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	memoPtr := flag.String("memo", "", "A memo to include in the genesis pass's passpoint memo field")
+	pubKeyPtr := flag.String("pubkey", "", "A public key to include in the genesis pass's passpoint output")
+	flag.Parse()
+
+	if len(*memoPtr) == 0 {
+		log.Fatal("Memo required for genesis pass")
+	}
+
+	if len(*pubKeyPtr) == 0 {
+		log.Fatal("Public key required for genesis pass")
+	}
+
+	pubKeyBytes, err := base64.StdEncoding.DecodeString(*pubKeyPtr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	pubKey := ed25519.PublicKey(pubKeyBytes)
+
+	// create the passpoint
+	tx := NewConsideration(nil, pubKey, 0, 0, 0, *memoPtr)
+
+	// create the pass
+	targetBytes, err := hex.DecodeString(INITIAL_TARGET)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var target PassID
+	copy(target[:], targetBytes)
+	pass, err := NewPass(PassID{}, 0, target, PassID{}, []*Consideration{tx})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// weave it
+	targetInt := pass.Header.Target.GetBigInt()
+	ticker := time.NewTicker(30 * time.Second)
+done:
+	for {
+		select {
+		case <-ticker.C:
+			pass.Header.Time = time.Now().Unix()
+		default:
+			// keep hashing until proof-of-work is satisfied
+			idInt, _ := pass.Header.IDFast(0)
+			if idInt.Cmp(targetInt) <= 0 {
+				break done
+			}
+			pass.Header.Nonce += 1
+			if pass.Header.Nonce > MAX_NUMBER {
+				pass.Header.Nonce = 0
+			}
+		}
+	}
+
+	passJson, err := json.Marshal(pass)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s\n", passJson)
+}

ledger.go
@@ -1,0 +1,69 @@
+package passthread
+
+import (
+	"golang.org/x/crypto/ed25519"
+)
+
+// BranchType indicates the type of branch a particular pass resides on.
+// Only passes currently on the main branch are considered confirmed and only
+// considerations in those passes affect public key imbalances.
+// Values are: MAIN, SIDE, ORPHAN or UNKNOWN.
+type BranchType int
+
+const (
+	MAIN = iota
+	SIDE
+	ORPHAN
+	UNKNOWN
+)
+
+// Ledger is an interface to a ledger built from the most-work thread of passes.
+// It manages and computes public key imbalances as well as consideration and public key consideration indices.
+// It also maintains an index of the pass thread by height as well as branch information.
+type Ledger interface {
+	// GetThreadTip returns the ID and the height of the pass at the current tip of the main thread.
+	GetThreadTip() (*PassID, int64, error)
+
+	// GetPassIDForHeight returns the ID of the pass at the given pass thread height.
+	GetPassIDForHeight(height int64) (*PassID, error)
+
+	// SetBranchType sets the branch type for the given pass.
+	SetBranchType(id PassID, branchType BranchType) error
+
+	// GetBranchType returns the branch type for the given pass.
+	GetBranchType(id PassID) (BranchType, error)
+
+	// ConnectPass connects a pass to the tip of the pass thread and applies the considerations
+	// to the ledger.
+	ConnectPass(id PassID, pass *Pass) ([]ConsiderationID, error)
+
+	// DisconnectPass disconnects a pass from the tip of the pass thread and undoes the effects
+	// of the considerations on the ledger.
+	DisconnectPass(id PassID, pass *Pass) ([]ConsiderationID, error)
+
+	// GetPublicKeyImbalance returns the current imbalance of a given public key.
+	GetPublicKeyImbalance(pubKey ed25519.PublicKey) (int64, error)
+
+	// GetPublicKeyImbalances returns the current imbalance of the given public keys
+	// along with pass ID and height of the corresponding main thread tip.
+	GetPublicKeyImbalances(pubKeys []ed25519.PublicKey) (
+		map[[ed25519.PublicKeySize]byte]int64, *PassID, int64, error)
+
+	// GetConsiderationIndex returns the index of a processed consideration.
+	GetConsiderationIndex(id ConsiderationID) (*PassID, int, error)
+
+	// GetPublicKeyConsiderationIndicesRange returns consideration indices involving a given public key
+	// over a range of heights. If startHeight > endHeight this iterates in reverse.
+	GetPublicKeyConsiderationIndicesRange(
+		pubKey ed25519.PublicKey, startHeight, endHeight int64, startIndex, limit int) (
+		[]PassID, []int, int64, int, error)
+
+	// Imbalance returns the total current ledger imbalance by summing the imbalance of all public keys.
+	// It's only used offline for verification purposes.
+	Imbalance() (int64, error)
+
+	// GetPublicKeyImbalanceAt returns the public key imbalance at the given height.
+	// It's only used offline for historical and verification purposes.
+	// This is only accurate when the full pass thread is indexed (pruning disabled.)
+	GetPublicKeyImbalanceAt(pubKey ed25519.PublicKey, height int64) (int64, error)
+}

inconsiderable/pass-trail

checkpoints.go
@@ -1,0 +1,33 @@
+package passtrail
+
+import (
+	"fmt"
+)
+
+// CheckpointsEnabled can be disabled for testing.
+const CheckpointsEnabled = false
+
+// LatestCheckpointHeight is used to determine if the client is synced.
+const LatestCheckpointHeight = 0
+
+// Checkpoints are known height and pass ID pairs on the main trail.
+var Checkpoints map[int64]string = map[int64]string{
+	
+}
+
+// CheckpointCheck returns an error if the passed height is a checkpoint and the
+// passed pass ID does not match the given checkpoint pass ID.
+func CheckpointCheck(id PassID, height int64) error {
+	if !CheckpointsEnabled {
+		return nil
+	}
+	checkpointID, ok := Checkpoints[height]
+	if !ok {
+		return nil
+	}
+	if id.String() != checkpointID {
+		return fmt.Errorf("Pass %s at height %d does not match checkpoint ID %s",
+			id, height, checkpointID)
+	}
+	return nil
+}

consideration_test.go
@@ -1,0 +1,145 @@
+package passtrail
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"testing"
+
+	"golang.org/x/crypto/ed25519"
+)
+
+func TestConsideration(t *testing.T) {
+	// create a sender
+	pubKey, privKey, err := ed25519.GenerateKey(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// create a recipient
+	pubKey2, privKey2, err := ed25519.GenerateKey(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// create the unsigned consideration
+	tx := NewConsideration(pubKey, pubKey2, 0, 0, 0, "for lunch")
+
+	// sign the consideration
+	if err := tx.Sign(privKey); err != nil {
+		t.Fatal(err)
+	}
+
+	// verify the consideration
+	ok, err := tx.Verify()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !ok {
+		t.Errorf("Verification failed")
+	}
+
+	// re-sign the consideration with the wrong private key
+	if err := tx.Sign(privKey2); err != nil {
+		t.Fatal(err)
+	}
+
+	// verify the consideration (should fail)
+	ok, err = tx.Verify()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if ok {
+		t.Errorf("Expected verification failure")
+	}
+}
+
+func TestConsiderationTestVector1(t *testing.T) {
+	// create consideration for Test Vector 1
+	pubKeyBytes, err := base64.StdEncoding.DecodeString("80tvqyCax0UdXB+TPvAQwre7NxUHhISm/bsEOtbF+yI=")
+	if err != nil {
+		t.Fatal(err)
+	}
+	pubKey := ed25519.PublicKey(pubKeyBytes)
+
+	pubKeyBytes2, err := base64.StdEncoding.DecodeString("YkJHRtoQDa1TIKhN7gKCx54bavXouJy4orHwcRntcZY=")
+	if err != nil {
+		t.Fatal(err)
+	}
+	pubKey2 := ed25519.PublicKey(pubKeyBytes2)
+
+	tx := NewConsideration(pubKey, pubKey2, 0, 0, 0, "for lunch")
+	tx.Time = 1558565474
+	tx.Nonce = 2019727887
+
+	// check JSON matches test vector
+	txJson, err := json.Marshal(tx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(txJson) != `{"time":1558565474,"nonce":2019727887,"from":"80tvqyCax0UdXB+TPvAQwre7NxUHhISm/bsEOtbF+yI=",`+
+		`"to":"YkJHRtoQDa1TIKhN7gKCx54bavXouJy4orHwcRntcZY=","memo":"for lunch","series":1}` {
+		t.Fatal("JSON differs from test vector: " + string(txJson))
+	}
+
+	// check ID matches test vector
+	id, err := tx.ID()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if id.String() != "04c5193340be556888ef4e1c2bdad865b83b01aa637381a382afbdf1abaedb5f" {
+		t.Fatalf("ID %s differs from test vector", id)
+	}
+
+	// add signature from test vector
+	sigBytes, err := base64.StdEncoding.DecodeString("i3XHtB9CrWFB/B3UBNBFQRZD236NNZjvIBfvFPlKyFccW4BLwZ/xBZyxAzrRfY7TwbzsuMKxh5+oGgxx9FTzDw==")
+	if err != nil {
+		t.Fatal(err)
+	}
+	tx.Signature = Signature(sigBytes)
+
+	// verify the consideration
+	ok, err := tx.Verify()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !ok {
+		t.Errorf("Verification failed")
+	}
+
+	// re-sign the consideration with private key from test vector
+	privKeyBytes, err := base64.StdEncoding.DecodeString("EBQtXb3/Ht6KFh8/+Lxk9aDv2Zrag5G8r+dhElbCe07zS2+rIJrHRR1cH5M+8BDCt7s3FQeEhKb9uwQ61sX7Ig==")
+	if err != nil {
+		t.Fatal(err)
+	}
+	privKey := ed25519.PrivateKey(privKeyBytes)
+	if err := tx.Sign(privKey); err != nil {
+		t.Fatal(err)
+	}
+
+	// verify the consideration
+	ok, err = tx.Verify()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !ok {
+		t.Errorf("Verification failed")
+	}
+
+	// re-sign the consideration with the wrong private key
+	_, privKey2, err := ed25519.GenerateKey(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := tx.Sign(privKey2); err != nil {
+		t.Fatal(err)
+	}
+
+	// verify the consideration (should fail)
+	ok, err = tx.Verify()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if ok {
+		t.Errorf("Expected verification failure")
+	}
+}

genesis-pass/main.go
@@ -1,0 +1,81 @@
+package main
+
+import (
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"math/rand"
+	"time"
+
+	. "github.com/inconsiderable/pass-trail"
+	"golang.org/x/crypto/ed25519"
+)
+
+// Trail a genesis pass
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	memoPtr := flag.String("memo", "", "A memo to include in the genesis pass's passpoint memo field")
+	pubKeyPtr := flag.String("pubkey", "", "A public key to include in the genesis pass's passpoint output")
+	flag.Parse()
+
+	if len(*memoPtr) == 0 {
+		log.Fatal("Memo required for genesis pass")
+	}
+
+	if len(*pubKeyPtr) == 0 {
+		log.Fatal("Public key required for genesis pass")
+	}
+
+	pubKeyBytes, err := base64.StdEncoding.DecodeString(*pubKeyPtr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	pubKey := ed25519.PublicKey(pubKeyBytes)
+
+	// create the passpoint
+	tx := NewConsideration(nil, pubKey, 0, 0, 0, *memoPtr)
+
+	// create the pass
+	targetBytes, err := hex.DecodeString(INITIAL_TARGET)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var target PassID
+	copy(target[:], targetBytes)
+	pass, err := NewPass(PassID{}, 0, target, PassID{}, []*Consideration{tx})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// trail it
+	targetInt := pass.Header.Target.GetBigInt()
+	ticker := time.NewTicker(30 * time.Second)
+done:
+	for {
+		select {
+		case <-ticker.C:
+			pass.Header.Time = time.Now().Unix()
+		default:
+			// keep hashing until proof-of-work is satisfied
+			idInt, _ := pass.Header.IDFast(0)
+			if idInt.Cmp(targetInt) <= 0 {
+				break done
+			}
+			pass.Header.Nonce += 1
+			if pass.Header.Nonce > MAX_NUMBER {
+				pass.Header.Nonce = 0
+			}
+		}
+	}
+
+	passJson, err := json.Marshal(pass)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s\n", passJson)
+}

ledger.go
@@ -1,0 +1,69 @@
+package passtrail
+
+import (
+	"golang.org/x/crypto/ed25519"
+)
+
+// BranchType indicates the type of branch a particular pass resides on.
+// Only passes currently on the main branch are considered confirmed and only
+// considerations in those passes affect public key imbalances.
+// Values are: MAIN, SIDE, ORPHAN or UNKNOWN.
+type BranchType int
+
+const (
+	MAIN = iota
+	SIDE
+	ORPHAN
+	UNKNOWN
+)
+
+// Ledger is an interface to a ledger built from the most-work trail of passes.
+// It manages and computes public key imbalances as well as consideration and public key consideration indices.
+// It also maintains an index of the pass trail by height as well as branch information.
+type Ledger interface {
+	// GetTrailTip returns the ID and the height of the pass at the current tip of the main trail.
+	GetTrailTip() (*PassID, int64, error)
+
+	// GetPassIDForHeight returns the ID of the pass at the given pass trail height.
+	GetPassIDForHeight(height int64) (*PassID, error)
+
+	// SetBranchType sets the branch type for the given pass.
+	SetBranchType(id PassID, branchType BranchType) error
+
+	// GetBranchType returns the branch type for the given pass.
+	GetBranchType(id PassID) (BranchType, error)
+
+	// ConnectPass connects a pass to the tip of the pass trail and applies the considerations
+	// to the ledger.
+	ConnectPass(id PassID, pass *Pass) ([]ConsiderationID, error)
+
+	// DisconnectPass disconnects a pass from the tip of the pass trail and undoes the effects
+	// of the considerations on the ledger.
+	DisconnectPass(id PassID, pass *Pass) ([]ConsiderationID, error)
+
+	// GetPublicKeyImbalance returns the current imbalance of a given public key.
+	GetPublicKeyImbalance(pubKey ed25519.PublicKey) (int64, error)
+
+	// GetPublicKeyImbalances returns the current imbalance of the given public keys
+	// along with pass ID and height of the corresponding main trail tip.
+	GetPublicKeyImbalances(pubKeys []ed25519.PublicKey) (
+		map[[ed25519.PublicKeySize]byte]int64, *PassID, int64, error)
+
+	// GetConsiderationIndex returns the index of a processed consideration.
+	GetConsiderationIndex(id ConsiderationID) (*PassID, int, error)
+
+	// GetPublicKeyConsiderationIndicesRange returns consideration indices involving a given public key
+	// over a range of heights. If startHeight > endHeight this iterates in reverse.
+	GetPublicKeyConsiderationIndicesRange(
+		pubKey ed25519.PublicKey, startHeight, endHeight int64, startIndex, limit int) (
+		[]PassID, []int, int64, int, error)
+
+	// Imbalance returns the total current ledger imbalance by summing the imbalance of all public keys.
+	// It's only used offline for verification purposes.
+	Imbalance() (int64, error)
+
+	// GetPublicKeyImbalanceAt returns the public key imbalance at the given height.
+	// It's only used offline for historical and verification purposes.
+	// This is only accurate when the full pass trail is indexed (pruning disabled.)
+	GetPublicKeyImbalanceAt(pubKey ed25519.PublicKey, height int64) (int64, error)
+}